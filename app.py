8c87fa7fa15098464506aafda6da5248e00c6875
patricemassey1-ops <patrice.massey1@gmail.com>
2025-12-30 10:51:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -25,10 +25,10 @@ if "PLAFOND_CE" not in st.session_state:
 # =====================================================
 LOGOS = {
     "Nordiques": "Nordiques_Logo.png",
-    "Cracheurs": "Cracheurs.jpeg",  
+    "Cracheurs": "Cracheurs_Logo.png",  
     "Prédateurs": "Prédateurs_Logo.png",
     "Red Wings": "Red_Wings_Logo.png",
-    "Whalers": "Whalers_Logo.jpg",
+    "Whalers": "Whalers_Logo.png",
     "Canadiens": "Canadiens_Logo.png"
 }
 
