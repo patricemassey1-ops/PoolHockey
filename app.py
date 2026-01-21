048c67855323451adb7792e3ca6207bf1b06c24b
patricemassey1-ops <patrice.massey1@gmail.com>
2026-01-21 18:15:08 -0500

    ddd

--- app.py
+++ app.py
@@ -11086,3 +11086,4 @@ elif active_tab == "🧠 Recommandations":
 
     out = pd.DataFrame(rows).sort_values(by=["Équipe"], kind="mergesort").reset_index(drop=True)
     st.dataframe(out, use_container_width=True, hide_index=True)
+    
